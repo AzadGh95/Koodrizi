dd29093f008f72a131748153d3b844d475db58af
A.Ghasemi <a.ghassemi92@gmail.com>
2021-02-09 12:56:26 +0330

    افزودن کلاس راس مشتریان برای ذخیره ی اطلاعات راس گیری مشتریان

---
+++ WindowsFormsApp_Koodrizi/WindowsFormsApp_Koodrizi/Models/AvgPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Koodrizi.Models
+{
+    public class AvgPerson
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdPerson { get; set; }
+        [ForeignKey("IdPerson")]
+        public virtual Person Person { get; set; }
+        public virtual List<DKood> DetailKoodrizis { get; set; }
+        public decimal Bedehi { get; set; }
+        public DateTime AvgDate { get; set; }
+    }
+}
